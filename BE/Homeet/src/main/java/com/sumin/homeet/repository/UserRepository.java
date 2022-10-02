fabfff092f1f752ca36498a7cf5224e8142ee059
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-10-02 15:44:48 +0900

    Feat:kakao token append

---
+++ BE/Homeet/src/main/java/com/sumin/homeet/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sumin.homeet.repository;
+
+import com.sumin.homeet.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    List<User> findByEmail(String email);
+}
