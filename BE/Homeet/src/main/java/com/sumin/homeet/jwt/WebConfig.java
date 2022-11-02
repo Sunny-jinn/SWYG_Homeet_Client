c00791c5977144da8d227a64c81e0f3561ae8dc1
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-02 14:03:32 +0900

    refactor: CORS config & interceptor amend
    인터셉터 수정 및 cors관련 설정 수정

--- BE/Homeet/src/main/java/com/sumin/homeet/jwt/WebConfig.java
+++ BE/Homeet/src/main/java/com/sumin/homeet/jwt/WebConfig.java
@@ -4,23 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     private final AuthenticationInterceptor authenticationInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .excludePathPatterns("/oauth/**","/**");
-//                .addPathPatterns("/**");
+                .excludePathPatterns("/oauth/**")
+                .addPathPatterns("/**");
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://0.0.0.0:3000");
+                .exposedHeaders("X-AUTH-TOKEN")
+                .allowedOrigins("http://0.0.0.0:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
