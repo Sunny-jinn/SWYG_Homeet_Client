3dc1be8ff3b42628364807b027dab61a123818ef
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-04 16:48:56 +0900

    Homeet Client

--- BE/Homeet/src/main/java/com/sumin/homeet/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sumin.homeet.repository;
-
-import com.sumin.homeet.domain.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note,Long> {
-}
